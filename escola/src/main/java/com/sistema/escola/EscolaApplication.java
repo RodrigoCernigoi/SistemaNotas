4854c8ed528160c94ff3925c7f080837701d7728
Rodrigo <81534029+RodrigoCernigoi@users.noreply.github.com>
2023-11-21 14:09:26 -0300

    Add files via upload
    Refinamento das telas.

--- escola/src/main/java/com/sistema/escola/EscolaApplication.java
+++ escola/src/main/java/com/sistema/escola/EscolaApplication.java
@@ -8,6 +8,8 @@ public class EscolaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EscolaApplication.class, args);
+                System.out.println("--- Link principal de acesso: http://localhost:8080/main/home");
+                
 	}
 
 }
